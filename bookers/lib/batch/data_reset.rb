6d921febf452c08f0b7a253f3e7264effe9fce9e
tmy628 <tmy6h1r@gmail.com>
2021-04-09 02:29:25 +0000

    [Fix/Edit] 削除プログラムの修正/削除プログラムが読み込まれるように設定

--- bookers/lib/batch/data_reset.rb
+++ bookers/lib/batch/data_reset.rb
@@ -1,7 +1,8 @@
 class Batch::DataReset
   def self.data_reset
-  # 投稿を全て削除
-  Book.dalete_all
-  p "投稿を全て削除しました"
+    # 投稿を全て削除
+    Book.delete_all
+    p "投稿を全て削除しました"
+    # bundle exec rails runner Batch::DataReset.data_resetが上手くいかない → Book.delete_all p "投稿を全て削除しました" 正しく改行できていなかった
   end
 end
